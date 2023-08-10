91bd4683d9c51c63e0ec80f6e2c13255f3437ff1
Victor <cool.badmaev@bk.ru>
2023-08-10 18:39:53 +0300

    second dzu(upd)

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk','name', 'price', 'category',)
+    ordering = ('category',)
+    search_fields = ('name', 'description')
+
+
+
+
